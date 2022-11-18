dea0b14d0eecace37ccff86224bb163ce13ee52e
MarcusVsc1 <marcus.vasconcelos@estudante.ufjf.br>
2022-11-17 22:40:40 -0300

    Primeira versão

---
+++ src/main/java/model/Dicionario.java
@@ -0,0 +1,12 @@
+package model;
+
+import interfaces.IdiomaBasico;
+
+public class Dicionario {
+
+    private IdiomaBasico palavraBasica;
+    private IdiomaBasicoAdapter idiomaBasicoAdapter;
+
+
+
+}
